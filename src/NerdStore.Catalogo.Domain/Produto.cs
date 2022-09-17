63ed87e7347760bd3cb6326022d5569d610b2048
Victor Cobo <v.cobof@outlook.com>
2022-09-17 11:35:55 -0300

    Criados objetos do shared kernel

---
+++ src/NerdStore.Catalogo.Domain/Produto.cs
@@ -0,0 +1,14 @@
+﻿using NerdStore.Core;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class Produto : Entity, IAggregateRoot
+    {
+
+    }
+
+    public class Categoria : Entity
+    {
+
+    }
+}
