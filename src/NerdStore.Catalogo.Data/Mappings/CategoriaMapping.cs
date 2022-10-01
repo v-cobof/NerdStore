cb48eed9ca82378638728a410944c7977232ceae
Victor Cobo <v.cobof@outlook.com>
2022-10-01 19:15:17 -0300

    Criados mapeamentos entre as entidades e as tabelas

---
+++ src/NerdStore.Catalogo.Data/Mappings/CategoriaMapping.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NerdStore.Catalogo.Domain;
+
+namespace NerdStore.Catalogo.Data.Mappings
+{
+    public class CategoriaMapping : IEntityTypeConfiguration<Categoria>
+    {       
+        public void Configure(EntityTypeBuilder<Categoria> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(250)");
+
+            // Relação 1:N => 1 Categoria : N Produtos
+            builder.HasMany(c => c.Produtos)
+                .WithOne(p => p.Categoria)
+                .HasForeignKey(p => p.Id);
+        }
+    }
+}
