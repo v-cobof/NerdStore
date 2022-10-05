a6c6f48578bc4625fee3cf192499cb9b91768f2f
Victor Cobo <v.cobof@outlook.com>
2022-10-05 08:21:02 -0300

    Adicionadas views e controler da vitrine. Alterada página inicial Adicionada injeção de dependência do evento de estoque baixo

--- src/NerdStore.WebApp.MVC/Program.cs
+++ src/NerdStore.WebApp.MVC/Program.cs
@@ -50,7 +50,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Vitrine}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
--- src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
+++ src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
@@ -1,7 +1,9 @@
-﻿using NerdStore.Catalogo.Application.Services;
+﻿using MediatR;
+using NerdStore.Catalogo.Application.Services;
 using NerdStore.Catalogo.Data;
 using NerdStore.Catalogo.Data.Repository;
 using NerdStore.Catalogo.Domain;
+using NerdStore.Catalogo.Domain.Events;
 using NerdStore.Core.Bus;
 
 namespace NerdStore.WebApp.MVC.Setup
@@ -16,6 +18,8 @@ namespace NerdStore.WebApp.MVC.Setup
             services.AddScoped<IProdutoAppService, ProdutoAppService>();
             services.AddScoped<IEstoqueService, EstoqueService>();
             services.AddScoped<CatalogoContext>();
+
+            services.AddScoped<INotificationHandler<ProdutoEstoqueBaixoEvent>, ProdutoEventHandler>();
         }
     }
 }
