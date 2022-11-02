03c80b11952c8db2024b3128c86ec47fe45838b0
Victor Cobo <v.cobof@outlook.com>
2022-11-02 11:23:30 -0300

    Inicio da implementação do dominio de vendas com CQRS

---
+++ src/NerdStore.WebApp.MVC/Controllers/CarrinhoController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using NerdStore.Catalogo.Application.Services;
+using NerdStore.Core.Communication.Mediator;
+using NerdStore.Core.Messages.CommonMessages.Notifications;
+using NerdStore.Vendas.Application.Commands;
+using NerdStore.Vendas.Application.Queries;
+using NerdStore.Vendas.Application.Queries.ViewModels;
+
+namespace NerdStore.WebApp.MVC.Controllers
+{
+    public class CarrinhoController : Controller
+    {
+        private readonly IProdutoAppService _produtoAppService;
+
+        public CarrinhoController(INotificationHandler<DomainNotification> notifications,
+                                  IProdutoAppService produtoAppService,
+                                  IMediatorHandler mediatorHandler,
+                                  IPedidoQueries pedidoQueries) : base(notifications, mediatorHandler)
+        {
+            _produtoAppService = produtoAppService;
+            _mediatorHandler = mediatorHandler;
+            _pedidoQueries = pedidoQueries;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("meu-carrinho")]
+        public async Task<IActionResult> AdicionarItem(Guid id, int quantidade)
+        {
+            var produto = await _produtoAppService.ObterPorId(id);
+            if (produto == null) return BadRequest();
+
+            if (produto.QuantidadeNoEstoque < quantidade)
+            {
+                TempData["Erro"] = "Produto com estoque insuficiente";
+                return RedirectToAction("ProdutoDetalhe", "Vitrine", new { id });
+            }
+
+            var command = new AdicionarItemPedidoCommand(ClienteId, produto.Id, produto.Nome, quantidade, produto.Valor);
+            await _mediatorHandler.EnviarComando(command);
+
+            if (OperacaoValida())
+            {
+                return RedirectToAction("Index");
+            }
+
+            TempData["Erros"] = ObterMensagensErro();
+            return RedirectToAction("ProdutoDetalhe", "Vitrine", new { id });
+        }
+    }
+}
--- src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
+++ src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
@@ -5,6 +5,7 @@ using NerdStore.Catalogo.Data.Repository;
 using NerdStore.Catalogo.Domain;
 using NerdStore.Catalogo.Domain.Events;
 using NerdStore.Core.Bus;
+using NerdStore.Vendas.Application.Commands;
 
 namespace NerdStore.WebApp.MVC.Setup
 {
@@ -12,14 +13,19 @@ namespace NerdStore.WebApp.MVC.Setup
     {
         public static void RegisterServices(this IServiceCollection services)
         {
+            // mediator
             services.AddScoped<IMediatrHandler, MediatrHandler>();
 
+            // catalogo
             services.AddScoped<IProdutoRepository, ProdutoRepository>();
             services.AddScoped<IProdutoAppService, ProdutoAppService>();
             services.AddScoped<IEstoqueService, EstoqueService>();
             services.AddScoped<CatalogoContext>();
 
             services.AddScoped<INotificationHandler<ProdutoEstoqueBaixoEvent>, ProdutoEventHandler>();
+
+            // vendas
+            services.AddScoped<IRequestHandler<AdicionarItemPedidoCommand, bool>, PedidoCommandHandler>();
         }
     }
 }
