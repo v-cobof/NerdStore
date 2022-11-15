31301dab4c19c76becb580828572d3c3c316cf8a
Victor Cobo <v.cobof@outlook.com>
2022-11-15 14:59:19 -0300

    Repository do event sourcing

--- src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
+++ src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
@@ -7,6 +7,7 @@ using NerdStore.Catalogo.Data.Repository;
 using NerdStore.Catalogo.Domain;
 using NerdStore.Catalogo.Domain.Events;
 using NerdStore.Core.Communication.Mediator;
+using NerdStore.Core.Data.EventSourcing;
 using NerdStore.Core.Messages.CommonMessages.IntegrationEvents;
 using NerdStore.Core.Messages.CommonMessages.Notifications;
 using NerdStore.Pagamentos.AntiCorruption;
@@ -36,6 +37,7 @@ namespace NerdStore.WebApp.MVC.Setup
 
             // Event Sourcing
             services.AddSingleton<IEventStoreService, EventStoreService>();
+            services.AddSingleton<IEventSourcingRepository, EventSourcingRepository>();
 
             // catalogo
             services.AddScoped<IProdutoRepository, ProdutoRepository>();
