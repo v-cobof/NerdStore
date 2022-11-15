1746bb52625d9b37bf85834daae41142c5719755
Victor Cobo <v.cobof@outlook.com>
2022-11-15 15:00:54 -0300

    Utilização dos services do event store

--- src/NerdStore.Core/Communication/Mediator/MediatrHandler.cs
+++ src/NerdStore.Core/Communication/Mediator/MediatrHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using NerdStore.Core.Data.EventSourcing;
 using NerdStore.Core.Messages;
 using NerdStore.Core.Messages.CommonMessages.Notifications;
 using System;
@@ -12,10 +13,12 @@ namespace NerdStore.Core.Communication.Mediator
     public class MediatrHandler : IMediatorHandler
     {
         private readonly IMediator _mediator;
+        private readonly IEventSourcingRepository _eventSourcingRepository;
 
-        public MediatrHandler(IMediator mediator)
+        public MediatrHandler(IMediator mediator, IEventSourcingRepository eventSourcingRepository)
         {
             _mediator = mediator;
+            _eventSourcingRepository = eventSourcingRepository;
         }
 
         public async Task<bool> EnviarComando<T>(T comando) where T : Command
@@ -26,6 +29,9 @@ namespace NerdStore.Core.Communication.Mediator
         public async Task PublicarEvento<T>(T evento) where T : Event
         {
             await _mediator.Publish(evento);
+
+            if(!evento.GetType().BaseType.Name.Equals("DomainEvent"))
+                await _eventSourcingRepository.SalvarEvento(evento);
         }
 
         public async Task PublicarNotificacao<T>(T notificacao) where T : DomainNotification
