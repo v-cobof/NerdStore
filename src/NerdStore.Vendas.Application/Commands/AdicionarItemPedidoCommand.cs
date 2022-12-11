87fd314f23e4f88c498a8d16d59b5047e8e10935
Victor Cobo <v.cobof@outlook.com>
2022-12-10 23:42:14 -0300

    ajustes nas mensagens de validação do command

--- src/NerdStore.Vendas.Application/Commands/AdicionarItemPedidoCommand.cs
+++ src/NerdStore.Vendas.Application/Commands/AdicionarItemPedidoCommand.cs
@@ -2,6 +2,7 @@
 using FluentValidation;
 using FluentValidation.Results;
 using NerdStore.Core.Messages;
+using NerdStore.Vendas.Domain;
 
 namespace NerdStore.Vendas.Application.Commands
 {
@@ -31,31 +32,36 @@ namespace NerdStore.Vendas.Application.Commands
 
     public class AdicionarItemPedidoValidation : AbstractValidator<AdicionarItemPedidoCommand>
     {
+        public static string IdClienteErroMsg => "Id do cliente inválido";
+        public static string IdProdutoErroMsg => "Id do produto inválido";
+        public static string NomeErroMsg => "O nome do produto não foi informado";
+        public static string QtdMaxErroMsg => $"A quantidade máxima de um item é {Pedido.MAX_UNIDADES_ITEM}";
+        public static string QtdMinErroMsg => "A quantidade miníma de um item é 1";
+        public static string ValorErroMsg => "O valor do item precisa ser maior que 0";
+
         public AdicionarItemPedidoValidation()
         {
             RuleFor(c => c.ClienteId)
                 .NotEqual(Guid.Empty)
-                .WithMessage("Id do cliente inválido");
+                .WithMessage(IdClienteErroMsg);
 
             RuleFor(c => c.ProdutoId)
                 .NotEqual(Guid.Empty)
-                .WithMessage("Id do produto inválido");
+                .WithMessage(IdProdutoErroMsg);
 
             RuleFor(c => c.Nome)
                 .NotEmpty()
-                .WithMessage("O nome do produto não foi informado");
+                .WithMessage(NomeErroMsg);
 
             RuleFor(c => c.Quantidade)
                 .GreaterThan(0)
-                .WithMessage("A quantidade miníma de um item é 1");
-
-            RuleFor(c => c.Quantidade)
-                .LessThan(15)
-                .WithMessage("A quantidade máxima de um item é 15");
+                .WithMessage(QtdMinErroMsg)
+                .LessThanOrEqualTo(Pedido.MAX_UNIDADES_ITEM)
+                .WithMessage(QtdMaxErroMsg);
 
             RuleFor(c => c.ValorUnitario)
                 .GreaterThan(0)
-                .WithMessage("O valor do item precisa ser maior que 0");
+                .WithMessage(ValorErroMsg);
         }
     }
 }
